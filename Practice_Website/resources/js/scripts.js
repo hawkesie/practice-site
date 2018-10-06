cbf508a961b816a008410cdbf7100110efbb561e
Jordan Hawkes <jordanhawkes93@gmail.com>
2018-10-06 21:41:42 +1000

    Add files via upload

---
+++ Practice_Website/resources/js/scripts.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+
+	$('.js--section-features').waypoint(function(direction) {
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else{
+			$('nav').removeClass('sticky');
+		}
+	},{
+		offset:'60px;'
+	});
+
+	$('.js--scroll-to-plan').click(function(){
+		$('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);	
+	});
+
+	$('.js--scroll-to-start').click(function(){
+		$('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);	
+	});
+/*var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})*/
+
+
+});
+
