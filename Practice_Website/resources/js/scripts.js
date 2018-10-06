6d1456abcac98deb83b8334ce4b2222615762f30
Jordan Hawkes <jordanhawkes93@gmail.com>
2018-10-07 00:57:54 +1000

    Add files via upload
    jquery, responsive mobile changes

--- Practice_Website/resources/js/scripts.js
+++ Practice_Website/resources/js/scripts.js
@@ -18,11 +18,70 @@ $(document).ready(function() {
 	$('.js--scroll-to-start').click(function(){
 		$('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);	
 	});
-/*var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})*/
+
+    //Smooth scrolling funtion
+	$("a").on('click', function(event) {
+	    if (this.hash !== "") {
+	      // Prevent default anchor click behavior
+	      event.preventDefault();
+
+	      // Store hash
+	      var hash = this.hash;
+
+	      // Using jQuery's animate() method to add smooth page scroll
+	      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	      $('html, body').animate({
+	        scrollTop: $(hash).offset().top
+	      }, 800, function(){
+	   
+	        // Add hash (#) to URL when done scrolling (default click behavior)
+	        window.location.hash = hash;
+	      });
+	    } // End if
+  	});
+
+	//Animations on scroll
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	},
+	{
+		offset:'50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeIn');
+	},
+	{
+		offset:'50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');
+	},
+	{
+		offset:'50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated fadeIn');
+	},
+	{
+		offset:'50%'
+	});
+
+	$('.js--wp-5').waypoint(function(direction){
+		$('.js--wp-5').addClass('animated fadeIn');
+	},
+	{
+		offset:'50%'
+	});
+
+    /*Mobile*/
+    $('.js--nav-icon').click(function(){
+    	var nav = $('.js--main-nav');
+
+    	nav.slideToggle(200);
+    });
 
 
 });
